0e35db2ee5533ce9455cf66abb298ce11760e6c4
Boo Gafford <byoboo@gmail.com>
2021-03-27 18:06:53 -0500

    finished testing

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -9,29 +9,17 @@ use tiny_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello you Beautiful Bastard{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-// This function is called on panic
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     tiny_os::test_panic_handler(info)
 }
 
 #[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+fn test_println() {
+    println!("test_println output");
 }
