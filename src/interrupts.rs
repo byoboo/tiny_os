a88a3c7a5ed36dbe6f8c28b04f0e3316d81904cc
Boo Gafford <byoboo@gmail.com>
2021-03-31 16:08:12 -0500

    check in

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -36,7 +36,7 @@ extern "x86-interrupt" fn double_fault_handler(
 }
 
 extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: &mut InterruptStackFrame) {
-    print!(".");
+    print!("lo{}", "l");
 
     unsafe {
         PICS.lock()
