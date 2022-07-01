e3646b4e727f363137b0968249e4da88babae645
Boo Gafford <byoboo@gmail.com>
2022-07-01 16:24:18 -0500

    macros
    added  print macros and  panic handler

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,13 @@ use core::panic::PanicInfo;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    println!("Hello World{}", "!");
     loop {}
 }
